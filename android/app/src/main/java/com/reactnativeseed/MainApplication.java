4b281258bfb76a060f10b56fc512a505b21fdb5b
Juno <juno@clearsumm.it>
2017-10-26 10:13:44 -0400

    support multiple environments debug | staging | production

--- android/app/src/main/java/com/reactnativeseed/MainApplication.java
+++ android/app/src/main/java/com/reactnativeseed/MainApplication.java
@@ -3,6 +3,7 @@ package com.reactnativeseed;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.microsoft.codepush.react.CodePush;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
+                    new ReactNativeConfigPackage(),
                     new CodePush(BuildConfig.CODEPUSH_KEY, getApplicationContext(), BuildConfig.DEBUG)
             );
         }
