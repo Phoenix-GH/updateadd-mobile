f68a43d58e6821c5b8c2f0a068cce0a6cf1431d0
Juno <juno@clearsumm.it>
2017-11-09 13:12:38 -0500

    avoid App Store Review rejection when implementing CodePush

--- src/App.js
+++ src/App.js
@@ -1,42 +1,23 @@
 // @flow
 
 import React from 'react'
-import { AppRegistry, StyleSheet, View, Text } from 'react-native'
-import Modal from 'react-native-modalbox'
+import { AppRegistry } from 'react-native'
 import { Provider } from 'react-redux'
+
 import CodePush from 'react-native-code-push'
 
 import SentryUtil from './utils'
-import ProgressBar from './components/progress-bar'
 
 import { RootContainer } from './screens/index'
 import Store from './store'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  modal: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 300,
-  },
-})
-
-type AppState = {
-  showDownloadingModal : bool,
-  showInstalling : bool,
-  downloadProgress : number
+const codePushOptions = {
+  checkFrequency: CodePush.CheckFrequency.ON_APP_START,
+  installMode: CodePush.InstallMode.ON_NEXT_SUSPEND,
 }
 
 // If environment is not dev setup Sentry and configure with CodePush
-export class App extends React.Component <any, AppState> {
-  state = {
-    showDownloadingModal: false,
-    showInstalling: false,
-    downloadProgress: 0,
-  }
-
+export class App extends React.Component <any> {
   componentWillMount() {
     // On app load
     if (!__DEV__) {
@@ -50,95 +31,10 @@ export class App extends React.Component <any, AppState> {
     }
   }
 
-  componentDidMount() {
-    CodePush.sync({
-      updateDialog: true,
-      installMode: CodePush.InstallMode.IMMEDIATE,
-    }, (status) => {
-      switch (status) {
-        case CodePush.SyncStatus.DOWNLOADING_PACKAGE:
-          this.setState({ showDownloadingModal: true })
-          if (this._modal) {
-            this._modal.open()
-          }
-          break;
-        case CodePush.SyncStatus.INSTALLING_UPDATE:
-          this.setState({ showInstalling: true })
-          break;
-        case CodePush.SyncStatus.UPDATE_INSTALLED:
-          if (this._modal) {
-            this._modal.close()
-          }
-          this.setState({ showDownloadingModal: false })
-          break;
-        default:
-          break;
-      }
-    }, ({ receivedBytes, totalBytes }) => {
-      this.setState({
-        downloadProgress: (receivedBytes / totalBytes) * 100,
-      })
-    })
-  }
-
   store = Store.configureStore()
   _modal = null
 
   render() {
-    if (this.state.showDownloadingModal) {
-      return (
-        <View style={styles.container}>
-          <Modal
-            style={styles.modal}
-            backdrop={false}
-            ref={(c) => { this._modal = c }}
-            swipeToClose={false}
-          >
-            <View style={{
-              flex: 1,
-              alignSelf: 'stretch',
-              justifyContent: 'center',
-              padding: 20,
-              }}
-            >
-              {this.state.showInstalling
-                ?
-                  <Text style={{
-                    color: '#5067FF',
-                    textAlign: 'center',
-                    marginBottom: 15,
-                    fontSize: 15,
-                  }}
-                  >
-                    Installing update...
-                  </Text>
-                :
-                  <View style={{
-                  flex: 1,
-                  alignSelf: 'stretch',
-                  justifyContent: 'center',
-                  padding: 20,
-                  }}
-                  >
-                    <Text style={{
-                    color: '#5067FF',
-                    textAlign: 'center',
-                    marginBottom: 15,
-                    fontSize: 15,
-                    }}
-                    >
-                     Downloading update... {' '}
-                      {`${parseInt(this.state.downloadProgress, 10)} %`}
-                    </Text>
-                    <ProgressBar color="#5067FF" progress={parseInt(this.state.downloadProgress, 10)} />
-                  </View>
-              }
-            </View>
-          </Modal>
-        </View>
-      )
-    }
-
     return (
       <Provider store={this.store}>
         <RootContainer />
@@ -148,7 +44,7 @@ export class App extends React.Component <any, AppState> {
 }
 
 const start = () => {
-  AppRegistry.registerComponent('ReactNativeSeed', () => App)
+  AppRegistry.registerComponent('ReactNativeSeed', () => CodePush(codePushOptions)(App))
 }
 
 export default start
