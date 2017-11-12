36f3035ef9097a134e5e066bbc922f1a3d07a405
Juno <juno@clearsumm.it>
2017-11-12 14:44:30 -0500

    remove buddybuild prebuild script, use cicrleci for test

--- android/buddybuild_prebuild.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo 'Run "jest && flow && eslint ." before build in Android'
-yarn test
--- ios/buddybuild_prebuild.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo 'Run "jest && flow && eslint ." before build in iOS'
-yarn test
