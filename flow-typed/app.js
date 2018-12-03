599fd55c9472d96b047303e81cedb3e3f6f41196
Juno <juno@clearsumm.it>
2018-12-04 00:25:24 +0700

    update packages

--- flow-typed/app.js
+++ flow-typed/app.js
@@ -1,7 +1,5 @@
 // Define type aliases
 type StandardAction = {| type: string; payload: any; error?: boolean; meta?: any |}
-type GeneratorType = Generator<void, void, any>
-
 
 // Store States
 type NavStoreState = Object
@@ -21,21 +19,18 @@ type StoreState = {
 }
 
 // General Types
+declare var __DEV__ : string;
 
 type ErrorType = {
   message: string,
 }
 
-// Network
-declare var __DEV__ : string;
-
-// File types
-
-// Components
-
 // modules - Because flow contains about these modules and they are not flow-typed
-declare module 'react-navigation' {
-  declare module.exports : any;
+declare module 'react-redux' {
+  declare module.exports: any;
+}
+declare module 'redux-saga/utils' {
+  declare module.exports: any;
 }
 declare module 'redux-saga/effects' {
   declare module.exports : any;
