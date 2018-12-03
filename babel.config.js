df9273a58686c12b840714f3601a14eda4e68a63
Juno <juno@clearsumm.it>
2018-12-04 00:23:37 +0700

    update babel configuration

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: [
+    'react-native',
+    '@babel/preset-flow',
+    'module:metro-react-native-babel-preset',
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+  ],
+}
