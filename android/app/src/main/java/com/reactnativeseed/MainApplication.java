15ff9201c66de1224c9c18ce884aea4add5f875c
Juno <juno@clearsumm.it>
2018-12-06 14:12:43 +0700

    run react-native link, update project

--- android/app/src/main/java/com/reactnativeseed/MainApplication.java
+++ android/app/src/main/java/com/reactnativeseed/MainApplication.java
@@ -3,10 +3,6 @@ package com.reactnativeseed;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
-import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
-import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -14,6 +10,7 @@ import com.facebook.soloader.SoLoader;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.microsoft.codepush.react.CodePush;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -38,12 +35,14 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
-            new RNGestureHandlerPackage(),
-            new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appcenterCrashes_whenToSendCrashes)),
-            new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appcenterAnalytics_whenToEnableAnalytics)),
-            new AppCenterReactNativePackage(MainApplication.this),
                     new VectorIconsPackage(),
-                    new RNSentryPackage(MainApplication.this),
+                    new RNSentryPackage(),
+                    new RNGestureHandlerPackage(),
+                    new ReactNativeConfigPackage(),
+                    new CodePush(null, getApplicationContext(), BuildConfig.DEBUG),
+                    new RNGestureHandlerPackage(),
+                    new VectorIconsPackage(),
+                    new RNSentryPackage(),
                     new ReactNativeConfigPackage(),
                     new CodePush(BuildConfig.CODEPUSH_KEY, getApplicationContext(), BuildConfig.DEBUG)
             );
