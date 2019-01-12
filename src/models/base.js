d020c0ad465232d1dc0a6df7935873c6a67a83f6
Juno <juno@clearsumm.it>
2019-01-12 15:46:22 +0700

    model should have private properties, gettersand setters

--- src/models/base.js
+++ src/models/base.js
@@ -1,11 +1,25 @@
 // @flow
 
-export default class BaseModel {
-  id: string
+type BaseType = {
+  id: ?string
+}
 
-  constructor(data?: { id?: string } = {}) {
+export default class BaseModel {
+  _id: string
+
+  constructor(data?: BaseType = {
+    id: null,
+  }) {
     if (data) {
       this.id = data.id || ''
     }
+  }
+
+  set id(value: ?string) {
+    this._id = value || ''
+  }
+
+  get id(): string {
+    return this._id
   }
 }
--- src/models/user.js
+++ src/models/user.js
@@ -3,17 +3,28 @@
 import { BaseModel } from '.'
 
 type UserDataType = {
-  id?: string,
-  email?: string,
+  id: ?string,
+  email: ?string,
 }
 
 export default class User extends BaseModel {
-  email: string
+  _email: string
 
-  constructor(data?: UserDataType = {}) {
+  constructor(data?: UserDataType = {
+    id: null,
+    email: null,
+  }) {
     super(data)
     if (data) {
       this.email = data.email || ''
     }
+  }
+
+  set email(value: ?string) {
+    this._email = value || ''
+  }
+
+  get email(): string {
+    return this._email
   }
 }
