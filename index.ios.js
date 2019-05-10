5ba3b9398f882622a0c45abca8d03c38a3a9e9de
Corey Rice <CoreyAR@users.noreply.github.com>
2019-05-10 05:56:12 -0400

    upgrade to RN 0.58.6 (#3)
    * start to upgrade to 0.58.6
    
    * babel-polyfill

--- index.ios.js
+++ index.ios.js
@@ -1,4 +1,5 @@
 import 'react-devtools'
+import 'babel-polyfill'
 import start from './src/App'
 // Debug network calls
 // GLOBAL.XMLHttpRequest = GLOBAL.originalXMLHttpRequest || GLOBAL.XMLHttpRequest
