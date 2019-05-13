a37a7ebe054eebeca60e24470d1c426788f69dac
Juno <juno@clearsumm.it>
2019-05-13 16:47:37 +0700

    fix: storybook failed to bundle when specify --projectRoot with RN 0.59.+

--- metro.config.js
+++ metro.config.js
@@ -5,7 +5,22 @@
  * @format
  */
 
+const path = require('path')
+
 module.exports = {
+  projectRoot: path.resolve(__dirname, '.'),
+
+  watchFolders: [
+    path.resolve(__dirname, 'node_modules'),
+  ],
+
+  resolver: {
+    // https://github.com/facebook/metro/issues/1#issuecomment-453450709
+    extraNodeModules: new Proxy({}, {
+      get: (target, name) => path.join(process.cwd(), `node_modules/${name}`),
+    }),
+  },
+
   transformer: {
     getTransformOptions: async () => ({
       transform: {
