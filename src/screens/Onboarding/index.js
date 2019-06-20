bda2837b23784ff973cc97ae3d2cb8b8f5028b5e
Phoenix <phoenix214@protonmail.com>
2019-06-20 13:56:46 -0700

    #UADD-8 added logo image #time 10m

---
+++ src/screens/Onboarding/index.js
@@ -0,0 +1,22 @@
+// @flow
+
+import React from 'react'
+import { View, Text } from 'react-native'
+import { connect } from 'react-redux'
+
+/* eslint-disable */
+class HelloWorldScreen extends React.Component<any> {
+  render() {
+    return (
+      <View style={{ alignItems: 'center' }}>
+        <Text>
+          Hello World
+        </Text>
+      </View>
+    )
+  }
+}
+/* eslint-enable */
+
+// $FlowFixMe
+export default connect((): Object => ({}))(HelloWorldScreen)
