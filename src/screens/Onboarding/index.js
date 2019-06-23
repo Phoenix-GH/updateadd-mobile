ec2f7f8ee1fb1026a7ffa98a24674d25e2e793fd
Ivan Jouikov <phoenix214@protonmail.com>
2019-06-23 11:42:48 -0700

    #UADD-12 added country modal & safeareaview #time 2h

--- src/screens/Onboarding/index.js
+++ src/screens/Onboarding/index.js
@@ -1,18 +1,26 @@
 // @flow
-
 import React from 'react'
-import { View, TouchableOpacity, Text, Image } from 'react-native'
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  Image,
+  StyleSheet,
+} from 'react-native'
 import { connect } from 'react-redux'
 import styled from 'styled-components'
+import CountryPicker, {
+  getAllCountries
+} from 'react-native-country-picker-modal'
+import SafeAreaView from 'react-native-safe-area-view'
 import { OnboardingCarousel } from '../../components'
-
 import gradient from '../../images/gradient.png'
 /* eslint-disable */
 
 const Container = styled(View)`
-	padding: 20px;
-	align-items: center;
-	flex: 1;
+  padding: 20px;
+  align-items: center;
+  flex: 1;
 `
 
 const OnboardingButton = styled(TouchableOpacity)`
@@ -38,19 +46,34 @@ const Gradient = styled(Image)`
   position: absolute;
 `
 
+const ModalView = styled(View)`
+  position: absolute;
+  top: -60;
+  left: -60;
+`
+
 class Onboarding extends React.Component<any> {
-  state = {
-    page: 0,
+  constructor(props) {
+    super(props)
+    this.state = {
+      cca2: 'US',
+      country: 'US',
+      page: 0,
+    }
   }
 
-	static navigationOptions = {
-		header: null,
-	}	
+  static navigationOptions = {
+    header: null,
+  } 
 
   onPressNext = () => {
     const { page } = this.state;
     if(page < 3) {
       this.setState({ page: page + 1 })
+    } else {
+      if(this.picker) {
+        this.picker.setState({ modalVisible: true })
+      }
     }
   }
 
@@ -61,19 +84,39 @@ class Onboarding extends React.Component<any> {
       buttonTitle = 'Select Your country'
     }
     return (
-      <Container>
-      	<OnboardingCarousel
-          activeIndex={page}
-          setIndex={(index) => this.setState({ page: index })}
-        />
-        <OnboardingButton onPress={this.onPressNext}>
-          <Gradient source={gradient} />
-          <ButtonText>{buttonTitle}</ButtonText>
-        </OnboardingButton>
-      </Container>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Container>
+          <OnboardingCarousel
+            activeIndex={page}
+            setIndex={(index) => this.setState({ page: index })}
+          />
+          <OnboardingButton onPress={this.onPressNext}>
+            <Gradient source={gradient} />
+            <ButtonText>{buttonTitle}</ButtonText>
+          </OnboardingButton>
+          <ModalView>
+            <CountryPicker
+              ref={(picker) => {this.picker = picker;}}
+              onChange={value => {
+                this.setState({ cca2: value.cca2, country: value })
+              }}
+              cca2={this.state.cca2}
+              translation="eng"
+              closeable={true}
+              style={listTheme}
+            />
+          </ModalView>
+        </Container>
+      </SafeAreaView>
     )
   }
 }
+
+const listTheme = StyleSheet.create({
+  letterText: {
+    color: 'blue',
+  }
+});
 /* eslint-enable */
 
 // $FlowFixMe
