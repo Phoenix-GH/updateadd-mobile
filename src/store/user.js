a6939a609baca42e2342ae413ee814e3a08672a4
Davis Hunt <vdh3@mac.com>
2019-07-03 19:15:58 -0700

    defined LoginPayload

--- src/store/user.js
+++ src/store/user.js
@@ -12,7 +12,7 @@ export const initialState: UserStoreState = {
 export const authDispatchers = {
   setPending: dispatchReducer<UserStoreState, boolean>(C.LOGIN_USER_PENDING, (state, pending): UserStoreState => ({ ...state, pending })),
   setError: dispatchReducer<UserStoreState, ?ErrorType>(C.LOGIN_USER_ERROR, (state, error): UserStoreState => ({ ...state, error })),
-  loginUser: dispatchReducer<UserStoreState, { email: string, password: string }>(C.LOGIN_USER, (state): UserStoreState => ({ ...state })),
+  loginUser: dispatchReducer<UserStoreState, UserLoginPayload>(C.LOGIN_USER, (state): UserStoreState => ({ ...state })),
   storeUser: dispatchReducer<UserStoreState, ?UserType>(C.LOGIN_STORE_USER, (state, user): UserStoreState => ({ ...state, user })),
 }
 
